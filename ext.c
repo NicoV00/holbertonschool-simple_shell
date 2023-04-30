262bacf89fe1a147e5cfcab7cf3650fb0eb93efa
root <Diego29012>
2023-04-30 07:16:47 -0700

    fixed

--- ext.c
+++ ext.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _getPATH - A function to gets the full value from.
- * enviromental variable PATH.
- * @env: The pointer to enviromental variables.
- * Return: All tokenized pathways for commands.
- */
+* _getPATH - A function to gets the full value from
+* enviromental variable PATH
+* @env: The pointer to enviromental variables
+* Return: All tokenized pathways for commands
+**/
 
 char **_getPATH(char **env)
 {
@@ -27,76 +27,75 @@ char **_getPATH(char **env)
 }
 
 
-/*
- * function that executes a command.
- * @command: The pointer to tokienized command
- * @name: The name of the shell.
- * @env: The pointer to the enviromental variables.
- * @cicles: Number of executed cicles.
- * Return: Nothing.
- */
+/**
+* function that executes a command
+* @command: The pointer to tokienized command
+* @name: The name of the shell
+* @env: The pointer to the enviromental variables
+* @cicles: Number of executed cicles
+* Return: Nothing
+**/
+
 void execute(char **command, char *name, char **env, int cicles)
 {
-	char **pathways = NULL, *full_path = NULL;
-	struct stat st;
-	unsigned int i = 0;
+		char **pathways = NULL, *full_path = NULL;
+		struct stat st;
+		unsigned int i = 0;
 
-	if (_strcmp(command[0], "env") != 0)
-	{
-		print_env(env);
-		return;
-	}
-
-	if (stat(command[0], &st) == 0)
-	{
-		if (execve(command[0], command, env) < 0)
+		if (_strcmp(command[0], "env") != 0)
 		{
-			perror(name);
+			print_env(env);
+			return;
+		}
+
+		if (stat(command[0], &st) == 0)
+		{
+			if (execve(command[0], command, env) < 0)
+			{
+				perror(name);
+				free_exit(command);
+			}
+		}
+		else
+		{
+			pathways = _getPATH(env);
+			while (pathways && pathways[i])
+			{
+					full_path = _strcat(pathways[i], "/");
+					full_path = _strcat(full_path, command[0]);
+					if (stat(full_path, &st) == 0)
+					{
+						if (execve(full_path, command, env) < 0)
+						{
+							perror(name);
+							free_exit(command);
+						}
+						return;
+					}
+					i++;
+			}
+			mserror(name, cicles, command);
 			free_exit(command);
 		}
-	}
-	else
-	{
-		pathways = _getPATH(env);
-		while (pathways && pathways[i])
-		{
-			full_path = _strcat(pathways[i], "/");
-			full_path = _strcat(full_path, command[0]);	
-			if (stat(full_path, &st) == 0)
-			{
-				if (execve(full_path, command, env) < 0)
-				{
-					perror(name);
-					free_exit(command);
-				}
-				return;
-			}
-			i++;
-		}
-		mserror(name, cicles, command);
-		free_exit(command);
-	}
 }
 
-/*
- * mserror - Function that prints message.
- * @name: Name of the shell.
- * @cicles: Number of cicles.
- * @command: Pointer to command.
- * Return: Nothing.
- */
+/**
+* mserror - A function that prints message not found
+* @name: The name of the shell
+* @cicles: Number of cicles
+* @command: The pointer to tokenized command
+* Return: Nothing
+**/
 
 void mserror(char *name, int cicles, char **command)
 {
-	char *num
+		char c;
 
-	num = _itoa(cicles);
-	write(STDOUT_FILENO, name, _strlen(name));
-	write(STDOUT_FILENO, ": ", 2);
-	write(STDOUT_FILENO, num, _strlen(num));
-	write(STDOUT_FILENO, ": ", 2);
-	write(STDOUT_FILENO, command[0], _strlen(command[0]));
-	write(STDOUT_FILENO, ": error not found\n", 12);
-	free(num);
-	free_exit(command);
+		c = cicles + '0';
+		write(STDOUT_FILENO, name, _strlen(name));
+		write(STDOUT_FILENO, ": ", 2);
+		write(STDOUT_FILENO, &c, 1);
+		write(STDOUT_FILENO, ": ", 2);
+		write(STDOUT_FILENO, command[0], _strlen(command[0]));
+		write(STDOUT_FILENO, ": not found\n", 12);
 }

16369ba4aa344afb61994ebdbe2f204df67097b3
root <Diego29012>
2023-04-30 17:36:20 -0700

    fixed

--- ext.c
+++ ext.c
@@ -84,18 +84,7 @@ void execute(char **command, char *name, char **env, int cicles)
  * @cicles: Number of cicles.
  * @command: The pointer to tokenized command.
  * Return: Nothing.
- */
-void mserror(char *name, int cicles, char **command)
-{
-	char c;
-	
-	c = cicles + '0';
-	write(STDOUT_FILENO, name, _strlen(name));
-	write(STDOUT_FILENO, ": ", 2);
-	write(STDOUT_FILENO, &c, 1);
-	write(STDOUT_FILENO, ": ", 2);
-	write(STDOUT_FILENO, command[0], _strlen(command[0]));
-	write(STDOUT_FILENO, ": not found\n", 12);
+ **/
 
 
 /**
